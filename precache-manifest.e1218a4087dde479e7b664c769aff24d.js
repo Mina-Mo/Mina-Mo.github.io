427d8b170a25f76d6ded4aa3aa09369b070892f8
Mina <mina.mounir@hotmail.com>
2019-05-04 14:57:44 +0300

    Updates

---
+++ precache-manifest.e1218a4087dde479e7b664c769aff24d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d3579f9ec90c6278a76072956296ca",
+    "url": "/Mina-Mo.github.io/index.html"
+  },
+  {
+    "revision": "93bb2f939519a4620fdc",
+    "url": "/Mina-Mo.github.io/static/css/main.19028aca.chunk.css"
+  },
+  {
+    "revision": "86aab86952e4ab2fd829",
+    "url": "/Mina-Mo.github.io/static/js/2.42176675.chunk.js"
+  },
+  {
+    "revision": "93bb2f939519a4620fdc",
+    "url": "/Mina-Mo.github.io/static/js/main.a82b1189.chunk.js"
+  },
+  {
+    "revision": "30b9399a4dffa36ed0d0",
+    "url": "/Mina-Mo.github.io/static/js/runtime~main.823e374c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Mina-Mo.github.io/precache-manifest.e1218a4087dde479e7b664c769aff24d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mina-Mo.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
